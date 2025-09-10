762985643561dda30c6315d357fbf9fa5a823b17
paresh patil <09patilparesh@gmail.com>
2025-09-10 15:27:04 +0530

    mm

--- routes/auth.js
+++ routes/auth.js
@@ -185,7 +185,8 @@ router.post("/login", validateLogin, async (req, res) => {
 router.get("/profile", async (req, res) => {
   try {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
-    
+    console.log("token>>>>>", token);
+    console.log("req.cookies.token>>>>>", req.cookies.token);
     if (!token) {
       return res.status(401).json({ 
         message: "Access token required" 
