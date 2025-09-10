c36b09e4a6b94869b3e90fd88fb1b47999c0c8c9
paresh patil <09patilparesh@gmail.com>
2025-09-10 15:35:54 +0530

    pushin

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,9 @@ const User = require('../models/User');
 const authenticateUser = async (req, res, next) => {
   try {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
-    
+
+    console.log("token>>>>>", token);
+    console.log("req.cookies.token>>>>>", req.cookies.token);
     if (!token) {
       return res.status(401).json({ 
         message: "Access token required" 
