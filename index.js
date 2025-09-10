6d8cb75e260cb4d63a0ff4891ad74ec4586d83b5
paresh patil <09patilparesh@gmail.com>
2025-09-10 14:11:29 +0530

    adding front end url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ["investandearnmoney.netlify.app", "http://localhost:5173"],
   credentials: true
 }));
 
@@ -59,7 +59,7 @@ connectDB();
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
