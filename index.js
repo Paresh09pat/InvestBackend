8ca8b26bc0cd2467a8abd112dd32c2b688757241
paresh patil <09patilparesh@gmail.com>
2025-09-15 16:03:22 +0530

    Update CORS configuration to include new production URL

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["https://investandearnmoney.netlify.app", "http://localhost:5173"],
+  origin: ["https://trdexa.com", "http://localhost:5173",],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
